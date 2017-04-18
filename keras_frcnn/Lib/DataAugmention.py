1069771f95f1bca38409e1274416c7172c2bae8a
Roei Herzig <roeih@traxretail.com>
2017-04-18 10:54:04 +0300

    change data augmention from class to a method

--- keras_frcnn/Lib/DataAugmention.py
+++ keras_frcnn/Lib/DataAugmention.py
@@ -4,68 +4,100 @@ import cv2
 __author__ = 'roeih'
 
 
-class DataAugmention(object):
+def augment_pascal_voc(img, img_data, config):
     """
-    This class is a Data augmentation
+    This function augment the data according to the config file
+    :param img: an image numpy type
+    :param img_data: image data
+    :param config: a config class
+    :return:
     """
+    img_data_aug = np.copy(img_data)
+    rows, cols = img.shape[:2]
 
-    def __init__(self, img, img_data, config):
-        self._img = img
-        self._config = config
-        self._img_data = img_data
-        # self._img_data = self._get_img_data_for_VG(img_data) if self._config.dataset == "VisualGenome" else img_data
+    if config.use_horizontal_flips and np.random.randint(0, 2) == 0:
+        img = cv2.flip(img, 1)
+        for bbox in img_data_aug['bboxes']:
+            x1 = bbox['x1']
+            x2 = bbox['x2']
+            bbox['x2'] = cols - x1
+            bbox['x1'] = cols - x2
 
-    def _get_img_data_for_VG(self, img_data):
-        """
-        
-        :param img_data: 
-        :return: 
-        """
+    if config.use_vertical_flips and np.random.randint(0, 2) == 0:
+        img = cv2.flip(img, 0)
+        for bbox in img_data_aug['bboxes']:
+            y1 = bbox['y1']
+            y2 = bbox['y2']
+            bbox['y2'] = rows - y1
+            bbox['y1'] = rows - y2
+
+    if config.random_rotate:
+        M = cv2.getRotationMatrix2D((cols / 2, rows / 2),
+                                    np.random.randint(-config.random_rotate_scale,
+                                                      config.random_rotate_scale), 1)
+        img = cv2.warpAffine(img, M, (cols, rows), flags=cv2.INTER_CUBIC,
+                             borderMode=cv2.BORDER_REPLICATE)
+
+        for bbox in img_data_aug['bboxes']:
+            K = np.array([[bbox['x1'], bbox['y1']], [bbox['x2'], bbox['y2']], [bbox['x1'], bbox['y2']],
+                          [bbox['x2'], bbox['y1']]])
+            K = cv2.transform(K.reshape(4, 1, 2), M)[:, 0, :]
+
+            (x1, y1) = np.min(K, axis=0)
+            (x2, y2) = np.max(K, axis=0)
+
+            bbox['x1'] = x1
+            bbox['x2'] = x2
+            bbox['y1'] = y1
+            bbox['y2'] = y2
+
+    return img_data_aug, img
 
 
-    def augment(self):
-        """
-        This function augment the Data according to the config file
-        :return:
-        """
-        img_data_aug = np.copy(self._img_data)
-        img = self._img
-        rows, cols = img.shape[:2]
+def augment_visual_genome(patch, object, config, mask):
+    """
+    This function augment the data according to the config file
+    :param patch: an image numpy type
+    :param object: object Visual Genome class
+    :param config: a config class
+    :param mask: a dict with {x1,x2,y1,y2} for each object
+    :return:
+    """
+    rows, cols = patch.shape[:2]
 
-        if self._config.use_horizontal_flips and np.random.randint(0, 2) == 0:
-            img = cv2.flip(img, 1)
-            for bbox in img_data_aug['bboxes']:
-                x1 = bbox['x1']
-                x2 = bbox['x2']
-                bbox['x2'] = cols - x1
-                bbox['x1'] = cols - x2
+    # new_patches = []
+    # new_labels = []
 
-        if self._config.use_vertical_flips and np.random.randint(0, 2) == 0:
-            img = cv2.flip(img, 0)
-            for bbox in img_data_aug['bboxes']:
-                y1 = bbox['y1']
-                y2 = bbox['y2']
-                bbox['y2'] = rows - y1
-                bbox['y1'] = rows - y2
+    if config.use_horizontal_flips and np.random.randint(0, 2) == 0:
+        new_patch = cv2.flip(patch, 1)
+        # new_patches.append(new_patch)
+        # new_labels.append(object.names[0])
 
-        if self._config.random_rotate:
-            M = cv2.getRotationMatrix2D((cols / 2, rows / 2),
-                                        np.random.randint(-self._config.random_rotate_scale,
-                                                          self._config.random_rotate_scale), 1)
-            img = cv2.warpAffine(img, M, (cols, rows), flags=cv2.INTER_CUBIC,
-                                 borderMode=cv2.BORDER_REPLICATE)
+    if config.use_vertical_flips and np.random.randint(0, 2) == 0:
+        new_patch = cv2.flip(patch, 0)
+        # new_patches.append(new_patch)
+        # new_labels.append(object.names[0])
 
-            for bbox in img_data_aug['bboxes']:
-                K = np.array([[bbox['x1'], bbox['y1']], [bbox['x2'], bbox['y2']], [bbox['x1'], bbox['y2']],
-                              [bbox['x2'], bbox['y1']]])
-                K = cv2.transform(K.reshape(4, 1, 2), M)[:, 0, :]
+    if config.random_rotate:
+        M = cv2.getRotationMatrix2D((cols / 2, rows / 2),
+                                    np.random.randint(-config.random_rotate_scale,
+                                                      config.random_rotate_scale), 1)
+        new_patch = cv2.warpAffine(patch, M, (cols, rows), flags=cv2.INTER_CUBIC,
+                                   borderMode=cv2.BORDER_REPLICATE)
 
-                (x1, y1) = np.min(K, axis=0)
-                (x2, y2) = np.max(K, axis=0)
+        # new_patches.append(new_patch)
+        # new_labels.append(object.names[0])
 
-                bbox['x1'] = x1
-                bbox['x2'] = x2
-                bbox['y1'] = y1
-                bbox['y2'] = y2
+        # K = np.array([[mask['x1'], mask['y1']], [mask['x2'], mask['y2']], [mask['x1'], mask['y2']],
+        #               [mask['x2'], mask['y1']]])
+        # K = cv2.transform(K.reshape(4, 1, 2), M)[:, 0, :]
 
-        return img_data_aug, img
+        # (x1, y1) = np.min(K, axis=0)
+        # (x2, y2) = np.max(K, axis=0)
+        #
+        # mask['x1'] = x1
+        # mask['x2'] = x2
+        # mask['y1'] = y1
+        # mask['y2'] = y2
+
+    return new_patch
