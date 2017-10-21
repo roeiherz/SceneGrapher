cf516d5406b7a07df0bd38701a065389db6d3e16
Roei Herzig <roeih@traxretail.com>
2017-10-22 01:01:32 +0300

    Train PredicateMask from the beginning

--- FeaturesExtraction/Lib/Config.py
+++ FeaturesExtraction/Lib/Config.py
@@ -13,14 +13,14 @@ class Config:
     def __init__(self, gpu_num):
 
         # Do we continuing to train or start from fresh
-        self.loading_model = True
+        self.loading_model = False
         self.loading_model_folder = "FilesManager/FeaturesExtraction/PredicatesMaskCNN/Mon_Sep_25_17:47:17_2017"
         self.loading_model_token = "scene_graph_base_module.visual_module.predicate_mask_dual_cnn"
         self.model_weights_name = 'model_vg_resnet50.hdf5'
         # Get the cached data-sets and cached hierarchy mapping and class counting
         self.use_cache_dir = False
         # Load weights
-        self.load_weights = True
+        self.load_weights = False
         # Replace the Dense layer
         self.replace_top = False
         # If we replace top, what is the old top number of classes
--- TrainPredicateMaskDualCNN.py
+++ TrainPredicateMaskDualCNN.py
@@ -380,7 +380,7 @@ if __name__ == '__main__':
     logger.log('Number of train detections before sorting negatives: {0} '
                'and test detections after sorting negatives {1}'.format(len(detections_train), len(detections_test)))
 
-    # Get new negative - positive ratio
+    # Get new negative - positive ratio and shuffle the data
     detections_train = pick_different_negative_sample_ratio(detections_train, ratio=RATIO)
     detections_test = pick_different_negative_sample_ratio(detections_test, ratio=RATIO)
     size_of_test = len(detections_train) / 3
@@ -469,14 +469,14 @@ if __name__ == '__main__':
         # In the summary, weights and layers from ResNet50 part will be hidden, but they will be fit during the training
         model.summary()
 
-    optimizer = Adam(1e-7)
+    optimizer = Adam(1e-6)
     model.compile(optimizer=optimizer,
                   loss='categorical_crossentropy', metrics=['accuracy'])
 
     callbacks = [ModelCheckpoint(net_weights_path, monitor='val_loss', save_best_only=True, verbose=0),
                  TensorBoard(log_dir="logs", write_graph=True, write_images=True),
                  CSVLogger(os.path.join(path, 'training.log'), separator=',', append=False),
-                 ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=0.00000001)]
+                 ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=0.0000001)]
 
     logger.log('Starting training')
     history = model.fit_generator(data_gen_train_vg, steps_per_epoch=len(train_imgs) / NUM_BATCHES, epochs=NUM_EPOCHS,
