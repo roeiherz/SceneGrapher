58595db86f4a01bc6ccc5fb4417f3a7d1a2489f4
Roei Herzig <roeih@traxretail.com>
2017-11-22 16:11:59 +0200

    Add objects mappings

--- LanguageModule/Word2Vec.py
+++ LanguageModule/Word2Vec.py
@@ -8,6 +8,7 @@ from Utils.Logger import Logger
 from FilesManager.FilesManager import FilesManager
 import cPickle
 import re
+import numpy as np
 
 __author__ = 'roeih'
 
@@ -23,7 +24,7 @@ def multi_word_replace(text, wordDic):
     return rc.sub(translate, text)
 
 
-def get_corpus_from_image_captioning(debug=1):
+def get_corpus_from_image_captioning(word2vec_mapping={}, debug=1):
     """
     This function will get the whole image captioning from the images and saves them as a corpus
     :return: train_sentences and test_sentences lists
@@ -52,7 +53,6 @@ def get_corpus_from_image_captioning(debug=1):
     # index for saving
     ind = 1
     Logger().log("Start creating corpus for train and test from image captioning")
-    word2vec_mapping = word2vec_mapping_func()
     train_sentences = []
     test_sentences = []
     for region in region_interest:
@@ -113,13 +113,66 @@ def get_train_and_test_word2vec_models(train_sentences, test_sentences):
 
     return train_model, test_model
 
+
+def get_objects_embedding(model, fname="object_embeddings"):
+    """
+    This function save objects embeddings
+    :param model: Word2Vec gensim model
+    :return:
+    """
+
+    # Check if pickles are already created
+    object_embeddings_path = FilesManager().get_file_path("language_module.word2vec.{}".format(fname))
+
+    if os.path.isfile(object_embeddings_path):
+        Logger().log('Files {0} are already exist'.format(object_embeddings_path))
+        object_embeddings = FilesManager().load_file("language_module.word2vec.{}".format(fname))
+        return object_embeddings
+
+    # Load hierarchy_mappings
+    hierarchy_mapping_objects = file_manager.load_file("data.visual_genome.hierarchy_mapping_objects")
+    hierarchy_mapping_predicates = file_manager.load_file("data.visual_genome.hierarchy_mapping_predicates")
+
+    # Get the inverse hierarchy_mapping_predicates
+    inv_hierarchy_mapping_objects = {v: k for k, v in hierarchy_mapping_objects.iteritems()}
+
+    predicates_embedding = []
+    for key, object in inv_hierarchy_mapping_objects.iteritems():
+        try:
+            word2vec = model.wv[object]
+            predicates_embedding.append(word2vec)
+        except Exception as e:
+            print("Problem with object:{0} with exception {1}".format(object, e))
+
+    object_embeddings = np.stack(predicates_embedding)
+    # Save pickle files
+    FilesManager().save_file("language_module.word2vec.{}".format(fname), object_embeddings)
+    return object_embeddings
+
+
 if __name__ == '__main__':
 
-    # Get data
-    train_sentences, test_sentences = get_corpus_from_image_captioning(debug=False)
+    # Define FileManager
+    file_manager = FilesManager()
+    # Define Logger
+    logger = Logger()
+
+    # Load Word2Vec mapping dict
+    word2vec_mapping = word2vec_mapping_func()
+
+    # ## Save from scratch models
+    # # Get data - sentences
+    # train_sentences, test_sentences = get_corpus_from_image_captioning(word2vec_mapping, debug=False)
+    # # Get models
+    # train_model, test_model = get_train_and_test_word2vec_models(train_sentences=train_sentences,
+    #                                                              test_sentences=test_sentences)
+
+    ## Load pre-trained models
     # Get models
-    train_model, test_model = get_train_and_test_word2vec_models(train_sentences=train_sentences,
-                                                                 test_sentences=test_sentences)
+    train_model, test_model = get_train_and_test_word2vec_models(train_sentences=None, test_sentences=None)
+    train_model.wv.similarity('woman', 'man')
+    # Save objects embeddings per model
+    object_embeddings = get_objects_embedding(train_model)
 
     exit()
     # Summarize the loaded model
