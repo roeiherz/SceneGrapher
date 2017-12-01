143ce6d4f068d70c18d3f1cd4517784bf9ffcc97
Roei Herzig <roeih@traxretail.com>
2017-12-01 22:56:11 +0200

    POS_NEG_RATIO=0.6

--- LanguageModule/LanguageTrain.py
+++ LanguageModule/LanguageTrain.py
@@ -31,7 +31,7 @@ TEST_ITERATIONS = 1
 CSVLOGGER = "training.log"
 # negative vs positive factor
 POS_NEG_FACTOR = 3.3
-POS_NEG_RATIO = 1
+POS_NEG_RATIO = 0.6
 
 
 def get_csv_logger(tf_graphs_path, timestamp):
